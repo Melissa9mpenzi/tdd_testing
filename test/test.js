3c36fce1f06b2273c7963787be7d112959328326
Melissa9mpenzi <melissampenzi@gmail.com>
2024-10-20 12:24:01 +0300

    edited tes.js page

--- test/test.js
+++ test/test.js
@@ -8,21 +8,18 @@ describe('Multiply function tests', function() {
   });
 
 
-describe('Multiply function tests', function() {
     it('should return 4 for multiply(2, 2)', function() {
         assert.equal(multiply(2, 2), 4);
     });
-  });
+  
 
 
-describe('Multiply function tests', function() {
     it('should return 9 for multiply(3, 3)', function() {
         assert.equal(multiply(3, 3), 9);
     });
-  });
+  
 
-describe('Multiply function tests', function() {
     it('should return 16 for multiply(4, 4)', function() {
         assert.equal(multiply(4, 4), 16);
     });
-  });
+  
